952a0a358bc18d4d8cf7102c4c5e7df3ed1fa97c
JasonHaque <zahinjason220434@gmail.com>
2019-11-14 10:15:51 +0600

    Added login Activity

---
+++ app/src/main/java/com/example/storybookapp/LogInActivity.kt
@@ -0,0 +1,12 @@
+package com.example.storybookapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class LogInActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_log_in)
+    }
+}
