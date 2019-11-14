fc2172eb9a97c61c4d043af98c3af9d4912bdb4a
JasonHaque <zahinjason220434@gmail.com>
2019-11-14 10:28:40 +0600

    added routes

--- app/src/main/java/com/example/storybookapp/Activities/MainActivity.kt
+++ app/src/main/java/com/example/storybookapp/Activities/MainActivity.kt
@@ -1,12 +1,16 @@
-package com.example.storybookapp
+package com.example.storybookapp.Activities
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.storybookapp.R
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val intent=Intent(this, LogInActivity::class.java)
+        startActivity(intent)
     }
 }
--- app/src/main/java/com/example/storybookapp/Activities/SignUpActivity.kt
+++ app/src/main/java/com/example/storybookapp/Activities/SignUpActivity.kt
@@ -1,7 +1,8 @@
-package com.example.storybookapp
+package com.example.storybookapp.Activities
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.storybookapp.R
 
 class SignUpActivity : AppCompatActivity() {
 
