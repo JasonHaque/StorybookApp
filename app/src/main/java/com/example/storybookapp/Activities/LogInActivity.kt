9c630117a569c9654e77a102e4ae5783ae264c1f
JasonHaque <zahinjason220434@gmail.com>
2019-11-16 15:47:35 +0600

    added new route

--- app/src/main/java/com/example/storybookapp/Activities/LogInActivity.kt
+++ app/src/main/java/com/example/storybookapp/Activities/LogInActivity.kt
@@ -1,14 +1,22 @@
 package com.example.storybookapp.Activities
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.storybookapp.R
+import kotlinx.android.synthetic.main.activity_log_in.*
 
 class LogInActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_log_in)
-        //Todo: implement login method
+        bindListeners()
+    }
+    private fun bindListeners(){
+        goto_sign_up_button.setOnClickListener {
+            val intent=Intent(this,SignUpActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
