a3fd558f2428b5c85154f0436c8a388fd7c17c51
JasonHaque <zahinjason220434@gmail.com>
2019-11-17 13:55:38 +0600

    added login field checker

--- app/src/main/java/com/example/storybookapp/Activities/LogInActivity.kt
+++ app/src/main/java/com/example/storybookapp/Activities/LogInActivity.kt
@@ -3,6 +3,7 @@ package com.example.storybookapp.Activities
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 import com.example.storybookapp.R
 import kotlinx.android.synthetic.main.activity_log_in.*
 
@@ -17,6 +18,15 @@ class LogInActivity : AppCompatActivity() {
         goto_sign_up_button.setOnClickListener {
             val intent=Intent(this,SignUpActivity::class.java)
             startActivity(intent)
+        }
+        log_in_button.setOnClickListener {
+            val email=log_in_mail.text.toString()
+            val password =log_in_password.text.toString()
+            if(email.isEmpty() || password.isEmpty()){
+                Toast.makeText(this,"Fill up the fields properly",Toast.LENGTH_SHORT).show()
+                return@setOnClickListener
+            }
+
         }
     }
 }
