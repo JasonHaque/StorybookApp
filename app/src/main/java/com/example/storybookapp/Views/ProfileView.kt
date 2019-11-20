9f41a74655889fc1b50eaacdb68ed14307cc407c
JasonHaque <zahinjason220434@gmail.com>
2019-11-20 19:52:47 +0600

    added new view binders

--- app/src/main/java/com/example/storybookapp/Views/ProfileView.kt
+++ app/src/main/java/com/example/storybookapp/Views/ProfileView.kt
@@ -3,11 +3,17 @@ package com.example.storybookapp.Views
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.storybookapp.R
+import com.google.firebase.auth.FirebaseAuth
+import kotlinx.android.synthetic.main.activity_profile_view.*
 
 class ProfileView : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_profile_view)
+        bindWidgets()
+    }
+    private fun bindWidgets(){
+        profile_view.text= FirebaseAuth.getInstance().currentUser?.email.toString()
     }
 }
