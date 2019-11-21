e5ad3dacbdbedef4e795eb0e1a1ef1b36e5346ee
JasonHaque <zahinjason220434@gmail.com>
2019-11-21 10:00:45 +0600

    added new views

--- app/src/main/java/com/example/storybookapp/Views/HomeView.kt
+++ app/src/main/java/com/example/storybookapp/Views/HomeView.kt
@@ -9,5 +9,6 @@ class HomeView : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home_view)
+        //Todo:Create a hamburger menu
     }
 }
