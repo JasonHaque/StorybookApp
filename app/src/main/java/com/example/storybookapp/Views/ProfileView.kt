895093aaeda500997789263935ee27adb1de9d4b
JasonHaque <zahinjason220434@gmail.com>
2019-11-21 09:58:16 +0600

    added logout mehtod

--- app/src/main/java/com/example/storybookapp/Views/ProfileView.kt
+++ app/src/main/java/com/example/storybookapp/Views/ProfileView.kt
@@ -1,7 +1,10 @@
 package com.example.storybookapp.Views
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import com.example.storybookapp.Activities.LogInActivity
 import com.example.storybookapp.R
 import com.google.firebase.auth.FirebaseAuth
 import kotlinx.android.synthetic.main.activity_profile_view.*
@@ -15,5 +18,11 @@ class ProfileView : AppCompatActivity() {
     }
     private fun bindWidgets(){
         profile_view.text= FirebaseAuth.getInstance().currentUser?.email.toString()
+        log_out_button.setOnClickListener {
+            FirebaseAuth.getInstance().signOut()
+            Toast.makeText(this,"Logging out",Toast.LENGTH_SHORT).show()
+            val intent=Intent(this,LogInActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
