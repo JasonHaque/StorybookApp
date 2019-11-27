28bf455b548dde88c09939c6e478c5506c9593aa
JasonHaque <zahinjason220434@gmail.com>
2019-11-27 21:47:34 +0600

    Added new listeners

--- app/src/main/java/com/example/storybookapp/Views/HomeView.kt
+++ app/src/main/java/com/example/storybookapp/Views/HomeView.kt
@@ -3,6 +3,7 @@ package com.example.storybookapp.Views
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.storybookapp.R
+import kotlinx.android.synthetic.main.activity_home_view.*
 
 class HomeView : AppCompatActivity() {
 
@@ -10,5 +11,17 @@ class HomeView : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home_view)
         //Todo:Create a hamburger menu
+        bindListeners()
+    }
+    private fun bindListeners(){
+        Create_new_story.setOnClickListener {
+
+        }
+        Current_Stories.setOnClickListener {
+
+        }
+        goto_profile.setOnClickListener {
+
+        }
     }
 }
