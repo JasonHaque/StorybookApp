92e94c599e64a6df616b1ffe536a2a45ee2b0670
JasonHaque <zahinjason220434@gmail.com>
2019-11-28 22:04:32 +0600

    Added new activity

---
+++ app/src/main/java/com/example/storybookapp/Activities/CurrentStoriesActivity.kt
@@ -0,0 +1,13 @@
+package com.example.storybookapp.Activities
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.storybookapp.R
+
+class CurrentStoriesActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_current_stories)
+    }
+}
