c47f6ce50aec3d4c56fc248146fb3e67852837d9
JasonHaque <zahinjason220434@gmail.com>
2019-11-29 19:20:49 +0600

    new routes

--- app/src/main/java/com/example/storybookapp/Views/HomeView.kt
+++ app/src/main/java/com/example/storybookapp/Views/HomeView.kt
@@ -3,6 +3,8 @@ package com.example.storybookapp.Views
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.storybookapp.Activities.CurrentStoriesActivity
+import com.example.storybookapp.Activities.NewStoryActivity
 import com.example.storybookapp.R
 import kotlinx.android.synthetic.main.activity_home_view.*
 
@@ -16,10 +18,11 @@ class HomeView : AppCompatActivity() {
     }
     private fun bindListeners(){
         Create_new_story.setOnClickListener {
-
+            val intent=Intent(this,NewStoryActivity::class.java)
+            startActivity(intent)
         }
         Current_Stories.setOnClickListener {
-
+            val intent=Intent(this,CurrentStoriesActivity::class.java)
         }
         goto_profile.setOnClickListener {
             val intent=Intent(this,ProfileView::class.java)
