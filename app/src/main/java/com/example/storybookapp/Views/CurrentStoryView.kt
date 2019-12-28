8ce850cfd790bfc86ae47a536ed9b5a066f1bb34
JasonHaque <zahinjason220434@gmail.com>
2019-12-28 17:51:26 +0600

    added new views

---
+++ app/src/main/java/com/example/storybookapp/Views/CurrentStoryView.kt
@@ -0,0 +1,13 @@
+package com.example.storybookapp.Views
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.storybookapp.R
+
+class CurrentStoryView : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_current_story_view)
+    }
+}
