6d9b4d7832a9113d768d0663fc7d82a4246b696b
JasonHaque <zahinjason220434@gmail.com>
2019-12-29 15:18:25 +0600

    new routes

--- app/src/main/java/com/example/storybookapp/Views/CurrentStoryView.kt
+++ app/src/main/java/com/example/storybookapp/Views/CurrentStoryView.kt
@@ -1,7 +1,9 @@
 package com.example.storybookapp.Views
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.storybookapp.Activities.NewStoryActivity
 import com.example.storybookapp.R
 import com.google.firebase.database.FirebaseDatabase
 import kotlinx.android.synthetic.main.activity_current_story_view.*
@@ -12,8 +14,8 @@ class CurrentStoryView : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_current_story_view)
-        var strName: String = intent.getStringExtra("StoryName")
-        var strDesc: String = intent.getStringExtra("StoryDesc")
+        val strName: String = intent.getStringExtra("StoryName")
+        val strDesc: String = intent.getStringExtra("StoryDesc")
         story_name_view.text = strName
         story_desc_view.text = strDesc
         bindListeners()
@@ -22,7 +24,8 @@ class CurrentStoryView : AppCompatActivity() {
 
     fun bindListeners(){
         go_back_to_new_Story.setOnClickListener {
-            //Todo:Add an Activity
+            val intent = Intent(this,NewStoryActivity::class.java)
+            startActivity(intent)
         }
 
     }
