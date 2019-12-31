bcea8af5ac1ea42bcf73f2934d9c6f4aa69f7c01
JasonHaque <zahinjason220434@gmail.com>
2019-12-31 23:20:53 +0600

    fixed references

--- app/src/main/java/com/example/storybookapp/Activities/ui/send/SendFragment.kt
+++ app/src/main/java/com/example/storybookapp/Activities/ui/send/SendFragment.kt
@@ -8,7 +8,7 @@ import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProviders
-import com.example.storybookapp.Activities.R
+//import com.example.storybookapp.Activities.R
 import com.example.storybookapp.R
 
 class SendFragment : Fragment() {
