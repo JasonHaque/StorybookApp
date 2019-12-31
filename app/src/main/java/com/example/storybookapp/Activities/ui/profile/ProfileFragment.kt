18df81b73e89b92351fbfae60a37e2d881be1822
afiatamanna06 <afiatamanna06@gmail.com>
2020-01-01 01:21:25 +0600

    Profile fragment added

---
+++ app/src/main/java/com/example/storybookapp/Activities/ui/profile/ProfileFragment.kt
@@ -0,0 +1,59 @@
+package com.example.storybookapp.Activities.ui.profile
+
+import android.content.Intent
+import androidx.lifecycle.ViewModelProviders
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Toast
+import androidx.core.content.ContextCompat
+import com.example.storybookapp.Activities.LogInActivity
+
+import com.example.storybookapp.R
+import com.example.storybookapp.Views.HomeView
+import com.google.firebase.auth.FirebaseAuth
+import kotlinx.android.synthetic.main.profile_fragment.*
+
+class ProfileFragment : Fragment() {
+
+    companion object {
+        fun newInstance() = ProfileFragment()
+    }
+
+    private lateinit var viewModel: ProfileViewModel
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.profile_fragment, container, false)
+    }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+        viewModel = ViewModelProviders.of(this).get(ProfileViewModel::class.java)
+        bindWidgets()
+        // TODO: Use the ViewModel
+    }
+
+    private fun bindWidgets(){
+        profile_view.text= FirebaseAuth.getInstance().currentUser?.email.toString()
+        log_out_button.setOnClickListener {
+            FirebaseAuth.getInstance().signOut()
+            Toast.makeText(this,"Logging out", Toast.LENGTH_SHORT).show()
+            val intent= Intent(this, LogInActivity::class.java)
+            ContextCompat.startActivity(intent)
+        }
+    }
+    override fun Fragment.onBackPressed() {
+        super.onBackPressed()
+        val intent = Intent(this@ProfileFragment, HomeView::class.java)
+        intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP)
+        ContextCompat.startActivity(intent)
+
+    }
+
+}
--- app/src/main/java/com/example/storybookapp/Activities/ui/share/ShareViewModel.kt
+++ app/src/main/java/com/example/storybookapp/Activities/ui/share/ShareViewModel.kt
@@ -7,7 +7,7 @@ import androidx.lifecycle.ViewModel
 class ShareViewModel : ViewModel() {
 
     private val _text = MutableLiveData<String>().apply {
-        value = "This is share Fragment"
+        value = "This is profile Fragment"
     }
     val text: LiveData<String> = _text
 }
