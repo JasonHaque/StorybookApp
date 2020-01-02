85f576a6a399f955640d13e34ed117ad5a1fb5d4
afiatamanna06 <afiatamanna06@gmail.com>
2020-01-02 12:05:12 +0600

    Reduncdancy removed

--- app/src/main/java/com/example/storybookapp/Activities/OpeningActivity.kt
+++ app/src/main/java/com/example/storybookapp/Activities/OpeningActivity.kt
@@ -39,7 +39,7 @@ class OpeningActivity : AppCompatActivity() {
         appBarConfiguration = AppBarConfiguration(
             setOf(
                 R.id.nav_home, R.id.nav_gallery, R.id.nav_slideshow,
-                R.id.nav_tools, R.id.nav_share, R.id.nav_send
+                R.id.nav_share, R.id.nav_send
             ), drawerLayout
         )
         setupActionBarWithNavController(navController, appBarConfiguration)
