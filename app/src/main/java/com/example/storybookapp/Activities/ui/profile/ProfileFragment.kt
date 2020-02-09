12344e8756e31d2dad73d8ca0c6dd39ab2e68506
afiatamanna06 <afiatamanna06@gmail.com>
2020-02-09 13:20:07 +0600

    Profile fragment updated

--- app/src/main/java/com/example/storybookapp/Activities/ui/profile/ProfileFragment.kt
+++ app/src/main/java/com/example/storybookapp/Activities/ui/profile/ProfileFragment.kt
@@ -1,20 +1,28 @@
 package com.example.storybookapp.Activities.ui.profile
 
+import android.app.Activity
 import android.content.Intent
-import androidx.lifecycle.ViewModelProviders
+import android.net.Uri
 import android.os.Bundle
-import androidx.fragment.app.Fragment
+import android.provider.MediaStore
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
-import androidx.core.content.ContextCompat
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProviders
 import com.example.storybookapp.Activities.LogInActivity
-
 import com.example.storybookapp.R
-import com.example.storybookapp.Views.HomeView
+import com.google.android.gms.tasks.Continuation
+import com.google.android.gms.tasks.Task
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.firestore.FirebaseFirestore
+import com.google.firebase.storage.FirebaseStorage
+import com.google.firebase.storage.StorageReference
+import com.google.firebase.storage.UploadTask
 import kotlinx.android.synthetic.main.profile_fragment.*
+import java.io.IOException
+import java.util.*
 
 class ProfileFragment : Fragment() {
 
@@ -32,6 +40,11 @@ class ProfileFragment : Fragment() {
         return inflater.inflate(R.layout.profile_fragment, container, false)
     }
 
+    private val PICK_IMAGE_REQUEST = 71
+    private var filePath: Uri? = null
+    private var firebaseStore: FirebaseStorage? = null
+    private var storageReference: StorageReference? = null
+
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
         viewModel = ViewModelProviders.of(this).get(ProfileViewModel::class.java)
@@ -40,6 +53,15 @@ class ProfileFragment : Fragment() {
     }
 
     private fun bindWidgets(){
+        firebaseStore = FirebaseStorage.getInstance()
+        storageReference = FirebaseStorage.getInstance().reference
+
+        profimage.setOnClickListener{
+            openFileChooser()
+        }
+        upload_button.setOnClickListener{
+            uploadImage()
+        }
         profile_view.text= FirebaseAuth.getInstance().currentUser?.email.toString()
         log_out_button.setOnClickListener {
             FirebaseAuth.getInstance().signOut()
@@ -48,12 +70,70 @@ class ProfileFragment : Fragment() {
             startActivity(intent)
         }
     }
-   /* override fun Fragment.onBackPressed() {
-        super.onBackPressed()
-        val intent = Intent(activity?.applicationContext, HomeView::class.java)
-        intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP)
-        startActivity(intent)
 
-    } */
+    private fun openFileChooser(){
+        val intent = Intent()
+        intent.type = "image/*"
+        intent.action = Intent.ACTION_GET_CONTENT
+        startActivityForResult(Intent.createChooser(intent, "Select Picture"), PICK_IMAGE_REQUEST)
+    }
+
+    private fun uploadImage(){
+        if(filePath != null){
+            val ref = storageReference?.child("uploads/" + UUID.randomUUID().toString())
+            val uploadTask = ref?.putFile(filePath!!)
+
+            val urlTask = uploadTask?.continueWithTask(Continuation<UploadTask.TaskSnapshot, Task<Uri>> { task ->
+                if (!task.isSuccessful) {
+                    task.exception?.let {
+                        throw it
+                    }
+                }
+                return@Continuation ref.downloadUrl
+            })?.addOnCompleteListener { task ->
+                if (task.isSuccessful) {
+                    val downloadUri = task.result
+                    addUploadRecordToDb(downloadUri.toString())
+                } else {
+                    // Handle failures
+                }
+            }?.addOnFailureListener{
+
+            }
+        }else{
+            Toast.makeText(this.context, "Please Upload an Image", Toast.LENGTH_SHORT).show()
+        }
+    }
+
+    private fun addUploadRecordToDb(uri: String){
+        val db = FirebaseFirestore.getInstance()
+
+        val data = HashMap<String, Any>()
+        data["imageUrl"] = uri
+
+        db.collection("posts")
+            .add(data)
+            .addOnSuccessListener { documentReference ->
+                Toast.makeText(this.context, "Saved to DB", Toast.LENGTH_LONG).show()
+            }
+            .addOnFailureListener { e ->
+                Toast.makeText(this.context, "Error saving to DB", Toast.LENGTH_LONG).show()
+            }
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (requestCode == PICK_IMAGE_REQUEST && resultCode == Activity.RESULT_OK) {
+            if(data == null || data.data == null){
+                return
+            }
+            val selectedImage = data.data
+            try {
+                profimage.setImageURI(selectedImage)
+            } catch (e: IOException) {
+                e.printStackTrace()
+            }
+        }
+    }
 
 }
